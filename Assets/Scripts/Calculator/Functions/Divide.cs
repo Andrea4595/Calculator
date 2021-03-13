a0d48f693a61ca1d74b56ef951905f107841a786
문기 정 <4595jmg@gmail.com>
2021-03-14 04:42:13 +0900

    Add calculator functions

---
+++ Assets/Scripts/Calculator/Functions/Divide.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Calculator.Functions
+{
+    public class Divide : Function
+    {
+        public override double GetResult(double a, double b)
+        {
+            if (b == 0)
+                return Mathf.Infinity;
+
+            return a / b;
+        }
+    }
+}
---
+++ Assets/Scripts/Calculator/Functions/Function.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Calculator.Functions
+{
+    public abstract class Function : MonoBehaviour
+    {
+        public abstract double GetResult(double a, double b);
+    }
+}
