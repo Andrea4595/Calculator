26446cad7eb0159d63274d6ff6746a103993d2e2
문기 정 <4595jmg@gmail.com>
2021-03-14 04:48:12 +0900

    Add button input

---
+++ Assets/Scripts/UI/Button.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI
+{
+    public abstract class Button : MonoBehaviour
+    {
+        public void Press() => Action();
+
+        protected abstract void Action();
+    }
+}
---
+++ Assets/Scripts/UI/NumberInput.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI
+{
+    public class NumberInput : Button
+    {
+        [SerializeField]
+        char _number;
+        [SerializeField]
+        Calculator.Terminal _terminal;
+
+        protected override void Action() => _terminal.PushNumber(_number);
+    }
+}
